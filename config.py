96e710d90db417db23432ae50a5bc0a6c749e77f
Michael Froehlich <m.froehlich@tum.de>
2018-03-11 17:11:50 -0700

    Add Flask Hello World server

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# TODO: Environment variables should be read from an .env file
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
