fba28f336029c24e0cc9e7c992db42a691ad5757
Michael Froehlich <m.froehlich@tum.de>
2018-03-11 17:13:49 -0700

    Add base template and index template

--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
 from flask import render_template
 from app import app
+from .forms import LoginForm
 
 
 @app.route('/')
 def index():
-    return "Hello World"
+    form = LoginForm()
+    if form.validate_on_submit():
+        return "Submitted"
+
+    return render_template('index.html')
 
