351108dd40ff5d14361d5a309348539c2b652cbf
Michael Froehlich <m.froehlich@tum.de>
2018-03-12 15:44:03 -0700

    Add registration form

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,1 +1,2 @@
 from .login_form import *
+from .registration_form import *
---
+++ app/forms/registration_form.py
@@ -0,0 +1,18 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from flask_wtf.html5 import EmailField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
+
+class RegistrationForm(Form):
+    firstname = StringField('Username', render_kw={'class': 'form-control', 'placeholder': 'John', 'required': True}, validators=[DataRequired('How should be call you? Please enter your first name.')])
+    lastname = StringField('Username', render_kw={'class': 'form-control', 'placeholder': 'Doe', 'required': True}, validators=[DataRequired('How should be call you? Please enter your last name.')])
+    email = EmailField('Email', render_kw={'class': 'form-control', 'placeholder': 'john@doe.com', 'required': True}, validators=[DataRequired('Please enter a valid email address.'), Email()])
+    password = PasswordField('Password', render_kw={'class': 'form-control', 'placeholder': '••••••••', 'required': True, 'pattern': '.{6,}', 'title': 'Minimum 6 characters'}, validators=[DataRequired('Please a password.')])
+    password2 = PasswordField('Confirm Password', render_kw={'class': 'form-control', 'placeholder': '••••••••', 'required': True, 'pattern': '.{6,}', 'title': 'Minimum 6 characters'}, validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register', render_kw={'class': 'btn btn-primary'} )
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
