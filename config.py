3c98b36e21c2dc0e3b5fb6eb1e7ea93f68933fea
Michael Froehlich <m.froehlich@tum.de>
2018-03-11 18:08:08 -0700

    Refactor config.py to use .env file

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 import os
+from dotenv import load_dotenv
+
+# Set basedir
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# TODO: Environment variables should be read from an .env file
+# Load environment variable from .env file
+# make sure that a .env file with the following variables exists
+env_file_path =  os.path.join(basedir, '.env')
+load_dotenv(dotenv_path=env_file_path)
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
-
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+WTF_CSRF_ENABLED = os.getenv('WTF_CSRF_ENABLED') != 'False'
+SECRET_KEY = os.getenv('SECRET_KEY')
