4527249571d924d53a4d650e97d6d1a0573b9d63
Michael Froehlich <m.froehlich@tum.de>
2018-03-12 16:13:10 -0700

    Style registration form

--- app/forms/registration_form.py
+++ app/forms/registration_form.py
@@ -5,12 +5,12 @@ from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models import User
 
 class RegistrationForm(Form):
-    firstname = StringField('Username', render_kw={'class': 'form-control', 'placeholder': 'John', 'required': True}, validators=[DataRequired('How should be call you? Please enter your first name.')])
-    lastname = StringField('Username', render_kw={'class': 'form-control', 'placeholder': 'Doe', 'required': True}, validators=[DataRequired('How should be call you? Please enter your last name.')])
+    firstname = StringField('First Name', render_kw={'class': 'form-control', 'placeholder': 'John', 'required': True}, validators=[DataRequired('How should be call you? Please enter your first name.')])
+    lastname = StringField('Last Name', render_kw={'class': 'form-control', 'placeholder': 'Doe', 'required': True}, validators=[DataRequired('How should be call you? Please enter your last name.')])
     email = EmailField('Email', render_kw={'class': 'form-control', 'placeholder': 'john@doe.com', 'required': True}, validators=[DataRequired('Please enter a valid email address.'), Email()])
-    password = PasswordField('Password', render_kw={'class': 'form-control', 'placeholder': '••••••••', 'required': True, 'pattern': '.{6,}', 'title': 'Minimum 6 characters'}, validators=[DataRequired('Please a password.')])
-    password2 = PasswordField('Confirm Password', render_kw={'class': 'form-control', 'placeholder': '••••••••', 'required': True, 'pattern': '.{6,}', 'title': 'Minimum 6 characters'}, validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register', render_kw={'class': 'btn btn-primary'} )
+    password = PasswordField('Password', render_kw={'class': 'form-control', 'placeholder': '6+ characters', 'required': True, 'pattern': '.{6,}', 'title': 'Minimum 6 characters'}, validators=[DataRequired('Please a password.')])
+    password2 = PasswordField('Confirm Password', render_kw={'class': 'form-control', 'placeholder': '6+ characters', 'required': True, 'pattern': '.{6,}', 'title': 'Minimum 6 characters'}, validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register', render_kw={'class': 'btn btn-primary btn-round'} )
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
