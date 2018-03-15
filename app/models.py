f8068175bde6b6f31d62c9ec4d8ca11a8ba9d2b5
Carina Sauter <carina.sauter@berkeley.edu>
2018-03-14 22:14:28 -0700

    added comments for clarification

--- app/models.py
+++ app/models.py
@@ -12,13 +12,11 @@ class User(UserMixin):
 		self.email = email
 		self.password_hash = password_hash
 
-	# def set_password(self, password):
- #        self.pw_hash = generate_password_hash(password)
 
- #    def check_password(self, password):
- #        return check_password_hash(self.pw_hash, password)
-
-def getUserByUsername(userid):
+""" Takes a username as parameter and checks in the database. If the user exists, 
+returns user object. If not, returns None.
+"""
+def getUserByUsername(query):
 	with sql.connect('database.db') as connection:
 		connection.row_factory = sql.Row
 		cursor = connection.cursor()
@@ -29,11 +27,12 @@ def getUserByUsername(userid):
 		else:
 			row = result[0]
 			user = User(row[0], username, row[2], row[3])
-			for item in row:
-				print(item)
 			return user
 
 
+""" Takes a userID as parameter and checks in the database. If the user exists, 
+returns user object. If not, returns None.
+"""
 def getUserById(query):
 	with sql.connect('database.db') as connection:
 		connection.row_factory = sql.Row
@@ -45,11 +44,20 @@ def getUserById(query):
 		else:
 			row = result[0]
 			user = User(query, row[1], row[2], row[3])
-			for item in row:
-				print(item)
 			return user
 
 
+""" Takes a username as parameter and 
+checks with user ID is associated with that username.
+Returns the userID. Assumes the username exists.
+"""
+def getUserID(query):
+	with sql.connect('database.db') as connection:
+		connection.row_factory = sql.Row
+		cursor = connection.cursor()
+		cursor.execute("SELECT * FROM users WHERE username=?", (query,))
+		result = cursor.fetchall()
+		return result[0][0]
 
 
 # def set_password(password):
@@ -80,19 +88,19 @@ def create_user(username, email, password_hash):
 		cursor.execute("INSERT INTO users (username, email, password_hash) VALUES (?,?, ?)",(username, email, password_hash))
 		connection.commit()
 
-def check_username_exists(query):
-	with sql.connect('database.db') as connection:
-		connection.row_factory = sql.Row
-		cursor = connection.cursor()
-		cursor.execute("SELECT * FROM users WHERE username=?", (query,))
-		result = cursor.fetchall()
-		if len(result) == 0:
-			# print("nothing found", file=sys.stderr)
-			return -1
-		else:
-			# print("something found", file=sys.stderr)
-			row = result[0]
-			return row[3] #returns password hash if exists
+# def check_username_exists(query):
+# 	with sql.connect('database.db') as connection:
+# 		connection.row_factory = sql.Row
+# 		cursor = connection.cursor()
+# 		cursor.execute("SELECT * FROM users WHERE username=?", (query,))
+# 		result = cursor.fetchall()
+# 		if len(result) == 0:
+# 			# print("nothing found", file=sys.stderr)
+# 			return -1
+# 		else:
+# 			# print("something found", file=sys.stderr)
+# 			row = result[0]
+# 			return row[3] #returns password hash if exists
 	
 # def retrieve_customer_id():
 # 	with sql.connect('database.db') as connection:
