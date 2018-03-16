5d6ce2af8049d3f56cd2847fe9d015f4d069a215
peterrowland <peter@peters-MacBook-Pro.local>
2018-03-16 12:34:50 -0700

    db create statement with foreign key constraints

--- app/views.py
+++ app/views.py
@@ -1,7 +1,30 @@
 from flask import render_template, redirect, request
 from app import app, models, db
+from flask_login import LoginManager, UserMixin
 # from .forms import
 # from .models import
+
+# flask-login
+# https://flask-login.readthedocs.io/en/latest/
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+# user model
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        self.name = "user" + str(id)
+        self.password = self.name + "_secret"
+
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+    
 
 # @app.route('/')
 # def index():
