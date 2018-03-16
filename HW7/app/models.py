490d2800ea8691b9dac389fe66a020d95af11e8d
jefferyycc <jefferyycc@gmail.com>
2018-03-15 20:35:37 -0700

    update login, sign_up (not yet0
    update signup, page-not-found, models.py & views.py

--- HW7/app/models.py
+++ HW7/app/models.py
@@ -1,19 +1,43 @@
 import sqlite3 as sql
 
-def login():
+# no need ? handle in the views.py
+#def login():
 
-def logout():
+# no need ? handle in the views.py
+#def logout():
 
-def signup():
+# no need ? handle in the views.py
+#def signup():
 
+# should we go with retrieve_trips() or retrieve_trip()? which one is better?
 def retrieve_trips():
 
-def create_trip():
+# change name to insert_trip
+#def create_trip():
+def insert_trip():
 
+# remove trip column from the table trips
 def delete_trip():
+    # DELETE FROM table_name WHERE some_column=some_value;
+    # ex: Delete from users where name = ‘eric’
 
-def retrieve_friends():
+# change name to retrieve_user
+# retrieve all user for the dropdown in the "creat"
+#def retrieve_friends():
+def retrieve_user():
+    with sql.connect("database.db") as con:
+        con.row_factory = sql.Row
+        cur = con.cursor()
+        result = cur.execute("select * from users").fetchall()
+    return result
 
-def create_user():
+# change name to insert_user
+# def create_user():
+def insert_user(username, password, first_name, last_name):
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO user (username, password, first_name, last_name) VALUES (?,?,?,?)", (username, password, first_name, last_name))
+        con.commit()
+    return cur.lastrowid
 
 def google_maps():
--- HW7/app/views.py
+++ HW7/app/views.py
@@ -4,21 +4,38 @@ from .forms import UserForm, TripForm, AddressForm
 # Access the models file to use SQL functions
 from .models import *
 
-@app.route('/index')
+# index page
 @app.route('/')
+@app.route('/index')
 def index():
     if 'username' in session:
-        return redirect('/trips')
+        username = escape(session['username'])
+        return render_template('trips.html', name=username)
     else:
         return render_template('login.html')
 
 @app.route('login', methods = ['GET', 'POST'])
 def login():
-    return None;
+    if request.method=='POST':
+        session['username'] = request.form['username']
+        session['password'] = request.form['password']
+        return redirect(url_for('index'))
 
 @app.route('/logout')
 def logout():
-    return None;
+    session.pop('username', None)
+    session.pop('password', None)
+    return redirect(url_for('index'))
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+	"""Register Form"""
+	if request.method == 'POST':
+		new_user = User(username=request.form['username'], password=request.form['password'])
+		db.session.add(new_user)
+		db.session.commit()
+		return render_template('login.html')
+	return render_template('signup.html')
 
 # need this?
 @app.route('/create_user', methods=['GET', 'POST'])
@@ -34,3 +51,6 @@ def create_trip(value):
     return None;
 
 # 404 errohandler
+@myapp.errorhandler(404)
+def page_not_found(error):
+	return render_template('page_not_found.html'), 404
