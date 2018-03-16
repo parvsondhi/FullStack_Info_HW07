512380127f5f09d882744269d2ccf61eac3e8609
Rabberina <sipian.wang@gmail.com>
2018-03-15 21:11:20 -0700

    Create the trip form

---
+++ app/forms/trip_form.py
@@ -0,0 +1,8 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import ValidationError, DataRequired
+from app.models import User
+
+class TripForm(Form):
+    name = StringField('Trip Name', render_kw={'class': 'form-control', 'placeholder': 'Spring Break Gppd Luck with Your Liver', 'required': True}, validators=[DataRequired('Please enter your trip name.')])
+    destination = StringField('Destination', render_kw={'class': 'form-control', 'placeholder': 'Floripa', 'required': True}, validators=[DataRequired('Please enter your travel destination')])
