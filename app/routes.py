8f653c0edb95465c7f63c6fd424bdb704a5bd884
peterrowland <peter@peters-MacBook-Pro.local>
2018-03-17 08:40:44 -0700

    finished login form and validation

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -18,7 +18,12 @@ def index():
     ]
     return render_template('index.html', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', user='t', form=form)
+    if form.validate_on_submit():
+        flash('Login user {}, remember={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('/index'))
+
+    flash('login fail')
+    return render_template('login.html', form=form)
