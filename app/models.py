36bca926aa59e92fbdb4c0010a961b6834a739d7
peterrowland <peter@peters-MacBook-Pro.local>
2018-03-17 10:42:45 -0700

    added trip successfully CLI

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(db.Model):
 class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tripname = db.Column(db.String(64), index=True, unique=True)
-    destination = db.Column(db.String(64), index=True, unique=True)
+    destination = db.Column(db.String(64), index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
