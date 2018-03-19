c600d85c0574c88a13ecc17c29b63bc401e65d31
peterrowland <peter@peters-MacBook-Pro.local>
2018-03-19 16:27:38 -0700

    populating select statement from database

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,8 @@
+from app import db
+from app.models import User
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, SelectField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired, NumberRange
 from wtforms.fields.html5 import EmailField
 
@@ -13,4 +16,8 @@ class TripForm(FlaskForm):
     tripname = StringField('tripname', validators=[DataRequired()])
     destination = StringField('destination', validators=[DataRequired()])
     # friend_id = IntegerField('friend_id', validators=[NumberRange(min=1, max=3, message='DEBUG: Can only be 1 - 3')])# TESTING -- Replace with Drop down
+    # friend_id = QuerySelectField(query_factory=lambda: )
+    friend_id = SelectField('friend', choices=[(r.id, r.username) for r in User.query.with_entities(User.id, User.username) ])
+
+
     submit = SubmitField('Create Trip')
