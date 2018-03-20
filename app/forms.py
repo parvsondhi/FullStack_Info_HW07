3bac0b05cb263ae1428d32d52a6c6bc62d84a131
michellecarney <michelle.r.carney@gmail.com>
2018-03-20 15:51:00 -0700

    added register a user, with testname and pw secret

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from flask_login import current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, SelectField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms.validators import ValidationError, DataRequired, NumberRange
 from wtforms.fields.html5 import EmailField
 
 class LoginForm(FlaskForm):
@@ -20,3 +20,13 @@ class TripForm(FlaskForm):
     friend_id = SelectField('friend_id', choices=[(r.id, r.username) for r in User.query.with_entities(User.id, User.username)], coerce=int)
     # friend_id = IntegerField('friend_id', validators=[NumberRange(min=1, max=2)])
     submit = SubmitField('Create Trip')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, TripForm
+from app.forms import LoginForm, TripForm, RegistrationForm
 from flask_login import current_user, login_user, login_required, logout_user
 from app.models import User, Trip
 from werkzeug.urls import url_parse
@@ -70,3 +70,17 @@ def delete_trip(value):
     db.session.commit()
 
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
