a0c206836b4e2387ff841a6c3b8ebc0a6abb0c5b
Xinhe Ren <jim.x.ren@berkeley.edu>
2018-03-19 22:10:58 -0700

    Changed to droptable if exists

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 -- Insert code to create Database Schema
 -- This will create your .db database file for use
-DROP TABLE users;
-DROP TABLE trips;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS trips;
 
 CREATE TABLE users(
     email TEXT,
