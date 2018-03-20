ef7c2202a1490c62b0001640530f40d4a8e691e1
Matt Bayley <bayley@berkeley.edu>
2018-03-20 14:39:32 -0700

    model fix

--- app/models.py
+++ app/models.py
@@ -4,20 +4,20 @@ def insert_user(username, password):
 	with sql.connect('database.db') as con:
 		cur = con.cursor()
         cur.execute("SELECT COUNT(*) FROM users WHERE username=?", [username])
-        if cur.fetchone()>0 return false
+        if cur.fetchone()[0]>0 return False
 		result = cur.execute(
             "INSERT INTO users (username, password)"
             " VALUES (?,?)"
             , )
 		con.commit()
-        return true
+        return True
 
 def login(username, password):
 	with sql.connect('database.db') as con:
 		cur = con.cursor()
         sql = "SELECT COUNT(*) FROM users WHERE (username=? AND password=?)",
         cur.execute(sql, (username, password))
-        return (cur.fetchone()>0)
+        return (cur.fetchone()[0]>0)
 
 def create_trip(name, destination, user1, user2):
     with sql.connect('database.db') as con:
@@ -26,13 +26,13 @@ def create_trip(name, destination, user1, user2):
             " VALUES (?, ?)")
         cur.execute(sql, (name, destination, user1, user2))
         con.commit()
-        return true
+        return True
 
 def delete_trip(id):
     with sql.connect('database.db') as con:
         cur = con.cursor()
         cur.execute("DELETE FROM trips WHERE id=?", [id])
-        val = cur.fetchone()
+        val = cur.fetchone()[0]
         cur.commit()
         return val
 
