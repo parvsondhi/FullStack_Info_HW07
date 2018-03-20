0b591655756ecee6ed676e23f354ecf9b3590316
Matt Bayley <bayley@berkeley.edu>
2018-03-20 14:40:48 -0700

    model fix

--- app/models.py
+++ app/models.py
@@ -1,20 +1,20 @@
 import sqlite3 as sql
 
 def insert_user(username, password):
-	with sql.connect('database.db') as con:
-		cur = con.cursor()
+    with sql.connect('database.db') as con:
+        cur = con.cursor()
         cur.execute("SELECT COUNT(*) FROM users WHERE username=?", [username])
         if cur.fetchone()[0]>0 return False
-		result = cur.execute(
+        result = cur.execute(
             "INSERT INTO users (username, password)"
             " VALUES (?,?)"
             , )
-		con.commit()
+        con.commit()
         return True
 
 def login(username, password):
-	with sql.connect('database.db') as con:
-		cur = con.cursor()
+    with sql.connect('database.db') as con:
+        cur = con.cursor()
         sql = "SELECT COUNT(*) FROM users WHERE (username=? AND password=?)",
         cur.execute(sql, (username, password))
         return (cur.fetchone()[0]>0)
