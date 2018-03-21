a15564973b0af36080d638b2c0b3fcb1e3eed86d
daphnejong <daphne.jong@berkeley.edu>
2018-03-20 20:17:33 -0700

    final final

--- travel/app/static/app.js
+++ travel/app/static/app.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
     $('select').material_select();
+
+    // $("#destination").click(function (e) {
+    // console.log("child clicked");
+	   // });
+ 
+	   // $(".container").click(function (e) {
+    // 	console.log("parent clicked");
+	   // });
+
+    // window.onclick = function(event) { 
+    // if (event.target != $('#destination')) {
+  //   $('#destination').blur(function() {
+  //   	console.log('yoyo');
+  // //       var e = $.Event( "keypress", { which: 13 } );
+		// // $('#destination').trigger(e);
+  //   }
+}
 });
