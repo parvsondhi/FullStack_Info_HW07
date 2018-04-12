0cc2fea2e333b36ad769d55228154ba0ffef523a
nolanchao <nolan_chao@mba.berkeley.edu>
2018-04-12 08:14:45 -0700

    Update forms.py
    Typo on TripForm. Changed to TripsForm.

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SelectField
 from flask_wtf.html5 import EmailField
 from wtforms.validators import DataRequired
 
-class TripForm(Form):
+class TripsForm(Form):
     trip_name =  StringField('trip_name', validators=[DataRequired()])
     destination = StringField('destination', validators=[DataRequired()])
     friend = SelectField('friend', choices=[])
